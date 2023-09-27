cd5b958c655d7c25970ad0b2ead3b08a90b41bf0
Shaz-hash <73599731+Shaz-hash@users.noreply.github.com>
2023-09-27 22:22:02 +0500

    updated Gradle File

--- examples/android/client/app/src/main/java/flwr/android_client/MainActivity.java
+++ examples/android/client/app/src/main/java/flwr/android_client/MainActivity.java
@@ -66,7 +66,35 @@ public class MainActivity extends AppCompatActivity {
         connectButton = (Button) findViewById(R.id.connect);
         trainButton = (Button) findViewById(R.id.trainFederated);
 
+<<<<<<< Updated upstream
         fc = new FlowerClient(this);
+=======
+        messageAdapter = new MessageAdapter(readStringFromFile( getApplicationContext() , "FlowerResults.txt")); // Create your custom adapter
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(messageAdapter);
+
+        requestPermission();
+
+        LifecycleOwner lifecycleOwner = this ;
+        WorkManager.getInstance(getApplicationContext()).getWorkInfosForUniqueWorkLiveData("my_unique_periodic_work").observe(lifecycleOwner, new Observer<List<WorkInfo>>() {
+            @Override
+            public void onChanged(List<WorkInfo> workInfos) {
+                if (workInfos.size() > 0) {
+                    WorkInfo info = workInfos.get(0);
+                    // You can recieve any message from the Worker Thread
+                    refreshRecyclerView();
+                }
+            }
+        });
+        // code for functionality of permission buttons :
+        batteryOptimisationButton = findViewById(R.id.battery_optimisation);
+        batteryOptimisationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleBatteryOptimization();
+            }
+        });
+>>>>>>> Stashed changes
     }
 
     public static void hideKeyboard(Activity activity) {
@@ -256,8 +284,142 @@ public class MainActivity extends AppCompatActivity {
                 requestObserver.onNext(c);
                 activity.setResultText("Response sent to the server");
             }
+<<<<<<< Updated upstream
             catch (Exception e){
                 Log.e(TAG, e.getMessage());
+=======
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception as needed
+        }
+    }
+
+
+
+    public void startWorker(View view) {
+
+        // ensuring all inputs are entered :
+
+        EditText deviceIdEditText = findViewById(R.id.device_id_edit_text);
+        EditText serverIPEditText = findViewById(R.id.serverIP);
+        EditText serverPortEditText = findViewById(R.id.serverPort);
+
+        // Get the text from the EditText widgets
+        String dataSlice = deviceIdEditText.getText().toString();
+        String serverIP = serverIPEditText.getText().toString();
+        String serverPort = serverPortEditText.getText().toString();
+
+        if (TextUtils.isEmpty(dataSlice) || TextUtils.isEmpty(serverIP) || TextUtils.isEmpty(serverPort)) {
+            // Display a toast message indicating that fields are omitted
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+        } else {
+
+            // Launching the Worker :
+            Constraints constraints = new Constraints.Builder()
+                    // Add constraints if needed (e.g., network connectivity)
+                    .build();
+
+            PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
+                    MyWorker.class, 15, TimeUnit.MINUTES)
+                    .setInitialDelay(0, TimeUnit.MILLISECONDS)
+                    .setInputData(new Data.Builder()
+                            .putString( "dataslice", deviceIdEditText.getText().toString() )
+                            .putString( "server", serverIPEditText.getText().toString())
+                            .putString( "port" , serverPortEditText.getText().toString())
+                            .build())
+                    .setConstraints(constraints)
+                    .build();
+
+            String uniqueWorkName = "my_unique_periodic_work";
+
+            WorkManager.getInstance(getApplicationContext())
+                    .enqueueUniquePeriodicWork(uniqueWorkName, ExistingPeriodicWorkPolicy.KEEP, workRequest);
+
+            // Providing user feedback, e.g., a toast message
+            Toast.makeText(this, "Worker started!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Listener function for the "Stop" button
+    public void stopWorker(View view) {
+        // Cancel the worker
+        WorkManager.getInstance(getApplicationContext()).cancelAllWork();
+        // Providing user feedback again, e.g., a toast message
+        Toast.makeText(this, "Worker stopped!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    // Another Listener function to refresh the updates :
+
+    public void refresh(View view)
+    {
+        refreshRecyclerView();
+    }
+
+    // Another Listener to clear the contents of the File :
+
+    public void clear(View view)
+    {
+        clearFileContents(getApplicationContext() , "FlowerResults.txt");
+    }
+
+
+    private void refreshRecyclerView() {
+        // Get messages from MessageRepository using the getMessagesArray method
+        List<String> messages = readStringFromFile( getApplicationContext() ,"FlowerResults.txt");
+
+        // Update the data source of your adapter with the new messages
+        messageAdapter.setData(messages);
+
+        // Notify the adapter that the data has changed
+        messageAdapter.notifyDataSetChanged();
+
+    }
+
+
+
+    // following code is for just the permission access from the user :
+    private void toggleBatteryOptimization() {
+        if (isBatteryOptimizationEnabled()) {
+            disableBatteryOptimization();
+        } else {
+            requestBatteryOptimization();
+        }
+    }
+
+    private boolean isBatteryOptimizationEnabled() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String packageName = getPackageName();
+            PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            return powerManager.isIgnoringBatteryOptimizations(packageName);
+        }
+        // Battery optimization is not available on versions prior to M, so return false.
+        return false;
+    }
+
+    private void disableBatteryOptimization() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
+    private void requestBatteryOptimization() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+//            startActivityForResult(intent, BATTERY_OPTIMIZATION_REQUEST_CODE);
+        }
+    }
+
+    public void createEmptyFile(String fileName) {
+        try {
+            File file = new File(fileName);
+
+            // Create the file if it doesn't exist
+            if (!file.exists()) {
+                file.createNewFile();
+>>>>>>> Stashed changes
             }
         }
     }
